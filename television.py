bfc4edee87c342e5aff7ebd73cedb1dce4b92967
AnhPhatN <128181010+AnhPhatN@users.noreply.github.com>
2023-11-21 11:10:42 -0600

    changed how volume is printed when TV is muted

--- television.py
+++ television.py
@@ -79,9 +79,9 @@ class Television:
 
     def __str__(self):
         if self.__muted == True:
-            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = 0'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         
-        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = [{self.__volume}'
 
 
 
